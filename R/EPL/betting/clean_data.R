e7f087100292b96ed699dc80d14aebe1bb30d1b6
Tuan Nguyen Doan <tuan.nguyen.doan@aya.yale.edu>
2019-10-26 16:50:49 -0700

    fix bugs and add comments
    fix bugs raised in Issue

--- R/EPL/betting/clean_data.R
+++ R/EPL/betting/clean_data.R
@@ -6,13 +6,16 @@
 #########################################
 library (dplyr)
 
-fixtures <- read.csv("fixtures.csv", stringsAsFactors = FALSE)
+link_fixture = "https://raw.githubusercontent.com/tuangauss/DataScienceProjects/master/data/fixtures.csv"
+link_history = "https://raw.githubusercontent.com/tuangauss/DataScienceProjects/master/data/history.csv"
+
+fixtures <- read.csv(link_fixture, stringsAsFactors = FALSE)
 
 # get the team
 teams <- unique(fixtures$HOME.TEAM)
 
 # extract historic results
-history <- read.csv("history.csv", stringsAsFactors = FALSE)
+history <- read.csv(link_history, stringsAsFactors = FALSE)
 
 # get info from the 2010 up to 2018
 seasons <- sapply(10:17, function(x) paste0(2000+x,'-',x+1))
@@ -25,17 +28,27 @@ teams[!teams %in% recent.pl$HomeTeam]
 unique(recent.pl$HomeTeam)
 
 # now we need to fix it
-pair_fix <- list(c('Manchester United', 'Man United'), c('Newcastle United', 'Newcastle'),
-                 c('Huddersfield Town', 'Huddersfield'), c('Wolverhampton Wanderers', 'Wolves'),
-                 c('Cardiff City', 'Cardiff'), c('Leicester City', 'Leicester'),
-                 c('Tottenham Hotspur', 'Tottenham'), c('West Ham United', 'West Ham'),
-                 c('Manchester City', "Man City"), c('Brighton and Hove Albion', 'Brighton'))
+pair_fix <- list(c('Manchester United', 'Man United'),
+                 c('Newcastle United', 'Newcastle'),
+                 c('Huddersfield Town', 'Huddersfield'),
+                 c('Wolverhampton Wanderers', 'Wolves'),
+                 c('Cardiff City', 'Cardiff'),
+                 c('Leicester City', 'Leicester'),
+                 c('Tottenham Hotspur', 'Tottenham'),
+                 c('West Ham United', 'West Ham'),
+                 c('Manchester City', "Man City"),
+                 c('Brighton and Hove Albion', 'Brighton'))
 
 # fix the recent.pl dataset
+# for name-conformity
 for (i in 1:length(pair_fix)){
   recent.pl <- recent.pl %>%
-    mutate(HomeTeam = replace(HomeTeam, HomeTeam == pair_fix[[i]][2], pair_fix[[i]][1]),
-           AwayTeam = replace(AwayTeam, AwayTeam == pair_fix[[i]][2], pair_fix[[i]][1]))
+    mutate(HomeTeam = replace(HomeTeam,
+                              HomeTeam == pair_fix[[i]][2],
+                              pair_fix[[i]][1]),
+           AwayTeam = replace(AwayTeam,
+                              AwayTeam == pair_fix[[i]][2],
+                              pair_fix[[i]][1]))
 }
 
 
@@ -58,5 +71,12 @@ hist_pair.pl <- recent.pl %>%
   group_by(HomeTeam, AwayTeam) %>%
   filter (HomeTeam %in% teams, AwayTeam %in% teams) %>%
   summarize (match = n(), ave_home_scored = mean(FTHG), ave_away_scored = mean(FTAG))
-  
-rm(history, seasons, pair_fix, ave_home, ave_away)
+
+# data set for new season
+# just clean the data name for readability
+new_season <- fixtures %>%
+  rename(HomeTeam = HOME.TEAM,
+         AwayTeam = AWAY.TEAM)
+
+# clean data form memory
+rm(history, seasons, recent.pl, pair_fix, ave_home, ave_away, fixtures)
