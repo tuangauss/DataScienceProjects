005db21f2b142d245c2a76f3244ff36f4dc3baee
Tuan Nguyen Doan <tuan.nguyen.doan@aya.yale.edu>
2020-12-29 16:56:44 -0800

    Update TrainingModule.py

--- Python/CollaborativeFiltering/TrainingModule.py
+++ Python/CollaborativeFiltering/TrainingModule.py
@@ -1,9 +1,12 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 import numpy as np
 import pandas as pd
 import io
 from collections import defaultdict
 from sklearn.metrics import mean_squared_error
-
 from numpy.linalg import solve
 np.random.seed(0)
 
@@ -48,3 +51,148 @@ def get_mse(pred, actual):
     pred = pred[actual.nonzero()].flatten()
     actual = actual[actual.nonzero()].flatten()
     return mean_squared_error(pred, actual)
+
+from numpy.linalg import solve
+
+class AlternatingLeastSquareMF():
+    def __init__(self, 
+                 ratings, 
+                 n_factors=40, 
+                 item_reg=0.0, 
+                 user_reg=0.0
+                 ):
+        """
+        Train a matrix factorization model to predict empty 
+        entries in a matrix.
+        
+        Params
+        ======
+        ratings : (ndarray)
+            User x Item matrix with corresponding ratings
+        
+        n_factors : (int)
+            Number of latent factors (k) to use in model
+        
+        item_reg : (float)
+            Regularization term for item latent factors
+        
+        user_reg : (float)
+            Regularization term for user latent factors
+        """
+        
+        self.ratings = ratings
+        self.n_users, self.n_items = ratings.shape
+        self.n_factors = n_factors
+        self.item_reg = item_reg
+        self.user_reg = user_reg
+
+    def alternating_step(self,
+                         latent_vectors,
+                         fixed_vecs,
+                         ratings,
+                         _lambda,
+                         type='user'):
+        """
+        One of the two ALS steps. Solve for the latent vectors
+        specified by type.
+        """
+        if type == 'user':
+            # Precompute
+            YTY = fixed_vecs.T.dot(fixed_vecs)
+            lambdaI = np.eye(YTY.shape[0]) * _lambda
+
+            for u in range(latent_vectors.shape[0]):
+                latent_vectors[u, :] = solve((YTY + lambdaI), 
+                                             ratings[u, :].dot(fixed_vecs))
+        elif type == 'item':
+            # Precompute
+            XTX = fixed_vecs.T.dot(fixed_vecs)
+            lambdaI = np.eye(XTX.shape[0]) * _lambda
+            
+            for i in range(latent_vectors.shape[0]):
+                latent_vectors[i, :] = solve((XTX + lambdaI), 
+                                             ratings[:, i].T.dot(fixed_vecs))
+        return latent_vectors
+
+        
+
+    def train(self, n_iter=10):
+        """ Train model for n_iter iterations from scratch."""
+        # initialize latent vectors
+        self.user_vecs = np.random.random((self.n_users, self.n_factors))
+        self.item_vecs = np.random.random((self.n_items, self.n_factors))
+        
+        ctr = 1
+        while ctr <= n_iter:
+            self.user_vecs = self.alternating_step(self.user_vecs, 
+                                                   self.item_vecs, 
+                                                   self.ratings, 
+                                                   self.user_reg, 
+                                                   type='user')
+            self.item_vecs = self.alternating_step(self.item_vecs, 
+                                                   self.user_vecs, 
+                                                   self.ratings, 
+                                                   self.item_reg, 
+                                                   type='item')
+            ctr += 1
+    
+    def predict_all(self):
+        """ Predict ratings for every user and item. """
+        predictions = np.zeros((self.user_vecs.shape[0], 
+                                self.item_vecs.shape[0]))
+        for u in range(self.user_vecs.shape[0]):
+            for i in range(self.item_vecs.shape[0]):
+                predictions[u, i] = self.predict(u, i)
+                
+        return predictions
+
+    def predict(self, u, i):
+        """ Single user and item prediction. """
+        return self.user_vecs[u, :].dot(self.item_vecs[i, :].T)
+    
+    def calculate_learning_curve(self, iter_array, test):
+        """
+        Keep track of MSE as a function of training iterations.
+        
+        Params
+        ======
+        iter_array : (list)
+            List of numbers of iterations to train for each step of 
+            the learning curve. e.g. [1, 5, 10, 20]
+        test : (2D ndarray)
+            Testing dataset (assumed to be user x item).
+        
+        The function creates two new class attributes:
+        
+        train_mse : (list)
+            Training data MSE values for each value of iter_array
+        test_mse : (list)
+            Test data MSE values for each value of iter_array
+        """
+        iter_array.sort()
+        self.train_mse =[]
+        self.test_mse = []
+        for (i, n_iter) in enumerate(iter_array):
+            self.train(n_iter)
+            predictions = self.predict_all()
+
+            self.train_mse += [get_mse(predictions, self.ratings)]
+            self.test_mse += [get_mse(predictions, test)]
+            print('Train mse: ' + str(self.train_mse[-1]))
+            print('Test mse: ' + str(self.test_mse[-1]))
+
+            
+            
+
+def plot_learning_curve(iter_array, model):
+    plt.plot(iter_array, model.train_mse, \
+             label='Training', linewidth=5)
+    plt.plot(iter_array, model.test_mse, \
+             label='Test', linewidth=5)
+
+
+    plt.xticks(fontsize=16);
+    plt.yticks(fontsize=16);
+    plt.xlabel('iterations', fontsize=30);
+    plt.ylabel('MSE', fontsize=30);
+    plt.legend(loc='best', fontsize=20);
