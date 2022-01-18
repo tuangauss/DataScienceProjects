113cb321567f2c76f66103e2f4ef137ab0940a53
Tuan Nguyen Doan <tuan.nguyen.doan@aya.yale.edu>
2022-01-17 20:30:16 -0800

    Update n_dimensionalNormal.py

--- Python/n_dimensionalNormal.py
+++ Python/n_dimensionalNormal.py
@@ -62,3 +62,39 @@ plt.xlim(1, 5000)
 plt.xlabel("dimensions")
 plt.title("Ratio between 2-standard devation boundaries and n as n increases", fontdict = {'fontsize': 16})
 plt.show()
+
+
+
+#### how many points lie in the 10% period or outside
+def get_pct_for_interval(n):
+  sample = np.random.normal(size=(N, n))
+  dist = np.square(np.linalg.norm(sample, axis = 1))
+  
+  lower_interval = np.count_nonzero(dist < n*0.95)
+  middle_interval = np.count_nonzero((dist >= n*0.95) & (dist <= n*1.05))
+  large_interval = np.count_nonzero(dist > n*1.05)
+  
+  return lower_interval/N, middle_interval/N, large_interval/N
+  
+lower_intervals = []
+middle_intervals = []
+large_intervals = []
+
+for n in n_range:
+  lower_interval, middle_interval, large_interval = get_pct_for_interval(n)
+  lower_intervals.append(lower_interval)
+  middle_intervals.append(middle_interval)
+  large_intervals.append(large_interval)
+
+plt.stackplot(n_range,
+              lower_intervals,
+              middle_intervals,
+              large_intervals,
+              labels=['d^2 < 0.95n',
+                      '0.95n <= d^2 <= 1.05n',
+                      'd^2 > 1.05n'])
+plt.legend()
+plt.xlabel("dimensions")
+plt.title("Probability that a sample point will be at some distance from the origin", fontdict = {'fontsize': 16})
+plt.ylim(0, 1)
+plt.xlim(1, 5000)
