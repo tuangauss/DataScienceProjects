86e630b77444a57bf37c7b21fa1e20117a0c6ba2
Tuan Nguyen Doan <tuan.nguyen.doan@aya.yale.edu>
2022-02-11 16:09:29 -0800

    Update optimal_dating.py

--- Python/optimal_dating.py
+++ Python/optimal_dating.py
@@ -44,3 +44,77 @@ def simulation_top_k(n, k, iters):
           result_m.append(success)
       result.append(np.mean(result_m))
   return result
+
+
+result = simulation_top_k(100, 1, iters)
+result_3 = simulation_top_k(100, 3, iters)
+result_5 = simulation_top_k(100, 5, iters)
+result_10 = simulation_top_k(100, 10, iters)
+
+
+plt.style.use('fivethirtyeight')
+plt.figure(figsize=(13,6))
+sns.scatterplot(np.arange(2, 101),y=result, label = "k = 1")
+sns.scatterplot(np.arange(2, 101),y=result_3, label = "k = 3")
+sns.scatterplot(np.arange(2, 101),y=result_5, label = "k = 5")
+sns.scatterplot(np.arange(2, 101),y=result_10, label = "k = 10")
+plt.grid(False)
+plt.title("Probability of finding top k partners\n by exploring first r values")
+plt.xlabel("r values")
+plt.ylabel("Probability")
+
+
+##############################
+# Top candidate with p success
+##############################
+
+def selection_algo_with_success_rate(array, m, p):
+  top_first_m = min(array[:(m-1)])
+  available_array = np.random.binomial(1, p, len(array))
+  #print(available_array)
+  # then for array[n:]
+  # we pick first k values that is greater than max_first_m 
+  #print("top first m", top_first_m)
+  #print(array[:(m-1)], array[m-1:])
+  inspect_array = array[m-1:]
+  inspect_available = available_array[m-1:]
+
+  if top_first_m == 1:
+      return 0
+  available_idx = np.where(inspect_available == 1)[0]
+  available_person = np.array(inspect_array)[available_idx]
+  pass_cand =  available_person[available_person < top_first_m]
+  #print(pass_cand)
+  if len(pass_cand) == 0:
+      return 0
+  accept = pass_cand[0]
+  if accept == 1:
+      return 1
+  return 0
+ 
+def simulate_with_success_rate(n, p, iters):
+  result = []
+  for m in range(2, n+1):
+      result_m = []
+      for i in range(iters):
+          order = perm_rank(n)
+          success = selection_algo_with_success_rate(order, m, p)
+          result_m.append(success)
+      result.append(np.mean(result_m))
+  return result
+
+result_avail_1 = simulate_with_success_rate(100, 1, iters)
+result_avail_2 = ssimulate_with_success_rate(100, 0.25, iters)
+result_avail_5 = simulate_with_success_rate(100, 0.5, iters)
+result_avail_7 = simulate_with_success_rate(100, 0.75, iters)
+
+plt.style.use('fivethirtyeight')
+plt.figure(figsize=(13,6))
+sns.scatterplot(np.arange(2, 101),y=result_avail_1, label = "p = 1")
+sns.scatterplot(np.arange(2, 101),y=result_avail_2, label = "p = 0.25")
+sns.scatterplot(np.arange(2, 101),y=result_avail_5, label = "p = 0.5")
+sns.scatterplot(np.arange(2, 101),y=result_avail_7, label = "p = 0.75")
+plt.title("Probability of finding top partner at different success rate\n by exploring first r values")
+plt.grid(False)
+plt.xlabel("r values")
+plt.ylabel("Probability")
